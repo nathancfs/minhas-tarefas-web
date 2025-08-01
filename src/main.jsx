d6f6c022b9832d3bbdd6a56db69189c5c4000d55
Nathan Cardoso <nathancf.santos@gmail.com>
2025-08-01 16:17:55 -0300

    Rollback forcado

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import TaskPage from "./pages/TaskPage.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/task",
-    element: <TaskPage />,
-  },
-]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
